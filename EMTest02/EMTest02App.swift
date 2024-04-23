c500bb299ace796348d7cf048dd7512a04132949
anton.a <anton.adamson@gmail.com>
2024-04-23 20:24:38 +0600

    Initial Commit

---
+++ EMTest02/EMTest02App.swift
@@ -0,0 +1,17 @@
+//
+//  EMTest02App.swift
+//  EMTest02
+//
+//  Created by Антон Адамсон on 23.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct EMTest02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
